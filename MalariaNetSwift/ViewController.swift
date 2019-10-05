56a07b1c6230f1fe980c4754c3539531a7b92bc0
soskill71 <kartikjodhpur@gmail.com>
2019-10-05 21:55:40 +0530

    Initial Commit

---
+++ MalariaNetSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MalariaNetSwift
+//
+//  Created by Kartik Kumar on 05/10/19.
+//  Copyright © 2019 Kartik Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
