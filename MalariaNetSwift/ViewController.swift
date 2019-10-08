1443074e625868eb605401d8770d5d67a066ec28
soskill71 <kartikjodhpur@gmail.com>
2019-10-08 20:57:59 +0530

    Add action sheet

--- MalariaNetSwift/ViewController.swift
+++ MalariaNetSwift/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cellImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func uploadButtonPressed(_ sender: Any) {
+        let actionSheet = UIAlertController(title: "Image Source", message: "Choose an image", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel , handler: nil))
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
 }
 
