76ea5fd518dc65f510895b2d74c30f501afda7e5
soskill71 <kartikjodhpur@gmail.com>
2019-10-21 22:30:03 +0530

    Implement CoreML model functionality

--- MalariaNetSwift/ViewController.swift
+++ MalariaNetSwift/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -38,8 +40,31 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
         cellImage.image = image
+        guard let convCIImage = CIImage(image: image) else {
+            fatalError("Image cannot be converted to CIImage")
+        }
+        detect(image: convCIImage)
         picker.dismiss(animated: true, completion: nil)
     }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: malaria().model) else {
+            fatalError("Cannot import the model")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let clf = request.results?.first as? VNClassificationObservation
+            print (clf?.identifier)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+    }
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
